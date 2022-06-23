4192bfd0d483bb5727538f4d8836fedd5247fb86
Rohit Gawai <106953373+Rohitgawai23@users.noreply.github.com>
2022-06-23 14:03:12 +0530

    Add files via upload

---
+++ evenoddSwitchcase.java
@@ -0,0 +1,98 @@
+import java.util.*;
+class evenoddSwitchcase
+{
+public static void main(String []args)
+{
+Scanner sc =new Scanner(System.in);
+System.out.print("\n\t\t\tenter the charactor= ");
+char ch=sc.next().charAt(0);
+
+switch(ch)
+{
+case 'a':
+	{
+	System.out.print("\t\t\tit is vowel");
+	break;
+	}
+	
+case 'e':
+	{
+	System.out.print("\t\t\tit is vowel");
+	break;
+	}
+case 'i':
+	{
+	System.out.print("\t\t\tit is vowel");
+	break;
+	}
+	
+case 'o':
+	{
+	System.out.print("\t\t\tit is vowel");
+	break;
+	}
+	
+case 'u':
+	{
+	System.out.print("\t\t\tit is vowel");
+	break;
+	}
+	
+case 'A':
+	{
+	System.out.print("\t\t\tit is vowel");
+	break;
+	}
+	
+case 'E':
+	{
+	System.out.print("\n\t\t\tit is vowel");
+	break;
+	}
+	
+case 'I':
+	{
+	System.out.print("\t\t\tit is vowel");
+	break;
+	}
+	
+case 'O':
+	{
+	System.out.print("\t\t\tit is vowel");
+	break;
+	}
+	
+case 'U':
+	{
+	System.out.print("\t\t\tit is vowel");
+	break;
+	}
+	
+default:
+	System.out.print("\t\t\tit is consonent");
+}
+
+
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
